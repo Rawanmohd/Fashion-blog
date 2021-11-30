b7f42a475d2f8adb81b4ce66153c22adadb45248
Layan <94787403+layanii@users.noreply.github.com>
2021-11-30 07:57:37 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fashion time</title>
+    <style><?php include"style.css"?> </style>
+</head>
+<body>
+
+<header id="head"> 
+      
+    <a href="home.php">
+        <img src="logo.png" alt="the website icon"
+   width="100" height="100"></a> <br>
+    <h1> Fashion time</h1> 
+    <ul>
+  <li><a href="home.php">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Brands</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
+</header>
+
+
+
+
+</body>
+</html>
